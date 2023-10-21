38f4634cfe244b0e97a171c979deb86a68c4de9c
Laurie Anne Laberge <laberge.la@gmail.com>
2023-10-21 19:32:38 -0400

    Implemented sign in functionality

--- app/src/main/java/com/example/coen390_safehit/SignIn.java
+++ app/src/main/java/com/example/coen390_safehit/SignIn.java
@@ -2,18 +2,25 @@ package com.example.coen390_safehit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignIn extends AppCompatActivity {
 
     // Layout elements
     TextInputEditText email, password;
     Button signIn, createAccount;
+    ProgressBar progressBar;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +28,18 @@ public class SignIn extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
         // Setup layout elements
         setupEditTextFields();
+        setupAuthentication();
         setupButtons();
+        setupProgressBar();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user != null) {
+            // Go to profile/main page
+        }
     }
 
     void setupEditTextFields(){
@@ -31,19 +49,40 @@ public class SignIn extends AppCompatActivity {
         password = findViewById(R.id.editTextSignInPassword);
     }
 
+    void setupAuthentication() {
+        mAuth = FirebaseAuth.getInstance();
+    }
+
     void setupButtons(){
         // Sign in button
         signIn = findViewById(R.id.signInButton);
         signIn.setOnClickListener(view -> {
             if(validEmailAndPassword()){
-                // Do something
+                progressBar.setVisibility(View.VISIBLE);
+                // Connect to account
+                mAuth.signInWithEmailAndPassword(String.valueOf(email.getText()), String.valueOf(password.getText()))
+                        .addOnCompleteListener(this, task -> {
+                            progressBar.setVisibility(View.GONE);
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(SignIn.this, "Sign in successful", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(SignIn.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
         // Create an account button
         createAccount = findViewById(R.id.createAnAccountButton);
         createAccount.setOnClickListener(view -> {
-
+            goToSignUp();
         });
+    }
+
+    void setupProgressBar() {
+        progressBar = findViewById(R.id.progressBar);
     }
 
     boolean validEmailAndPassword() {
@@ -55,5 +94,10 @@ public class SignIn extends AppCompatActivity {
 
     void showToast() {
         Toast.makeText(SignIn.this, "Please fill out all required fields", Toast.LENGTH_SHORT).show();
+    }
+
+    void goToSignUp() {
+        Intent signUp = new Intent(getApplicationContext(), SignUp.class);
+        startActivity(signUp);
     }
 }
