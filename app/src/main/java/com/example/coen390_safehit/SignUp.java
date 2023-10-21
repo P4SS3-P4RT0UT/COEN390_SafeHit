675a2a1dae705524f356fee7d47bfacc8847b353
Laurie Anne Laberge <laberge.la@gmail.com>
2023-10-21 18:20:12 -0400

    Setup sign up activity

--- app/src/main/java/com/example/coen390_safehit/SignUp.java
+++ app/src/main/java/com/example/coen390_safehit/SignUp.java
@@ -7,17 +7,22 @@ import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.Toast;
 
+
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
 
     TextInputEditText email, password;
     Button signUp;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        setupAuthentication();
         setupEditTextFields();
         setupButtons();
     }
@@ -31,9 +36,24 @@ public class SignUp extends AppCompatActivity {
         signUp = findViewById(R.id.signUpButton);
         signUp.setOnClickListener(view -> {
             if(validEmailAndPassword()){
-
+                mAuth.createUserWithEmailAndPassword(String.valueOf(email.getText()), String.valueOf(password.getText()))
+                        .addOnCompleteListener(this, task -> {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(SignUp.this, "Account created successfully", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(SignUp.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
+    }
+
+    void setupAuthentication() {
+        mAuth = FirebaseAuth.getInstance();
     }
 
     boolean validEmailAndPassword() {
