4b3d61fc25c6be3f5abdeecc04317944ae7f19a2
Hahahhaha12345 <92177243+Hahahhaha12345@users.noreply.github.com>
2023-11-12 22:20:34 -0500

    Add files via upload

--- app/src/main/java/com/example/coen390_safehit/view/SettingsActivity.java
+++ app/src/main/java/com/example/coen390_safehit/view/SettingsActivity.java
@@ -1,8 +1,6 @@
 package com.example.coen390_safehit.view;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,46 +12,34 @@ public class SettingsActivity extends AppCompatActivity {
 
     // Person id to identify user
     private String uid;
-
-    // Toolbar for back navigation
-    private Toolbar toolbar;
+    private String type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        // Setup the toolbar
-        setupToolBar();
         // Fetch the id from the previous activity
-        //uid = getIntent().getStringExtra("pid");
+        uid = getIntent().getStringExtra("pid");
+        type= getIntent().getStringExtra("type");
     }
 
     public void onLogOutClicked(View view) {
         goToSignIn();
     }
-
     public void onDeleteAccountClicked(View view) {
         // Delete account from database
     }
-
-    private void setupToolBar() {
-        toolbar = findViewById(R.id.toolbarSettings);
-        toolbar.setTitle("Settings");
-        setSupportActionBar(toolbar);
-
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-
-        //Return to previous page
-        toolbar.setNavigationOnClickListener(v -> finish());
-
+    public void onUpdateInfoClicked(View view) {
+        goToPersonalInformation();
     }
-
     public void goToSignIn() {
         Intent intent = new Intent(getApplicationContext(), SignIn.class);
+        startActivity(intent);
+    }
+    void goToPersonalInformation() {
+        Intent intent = new Intent(getApplicationContext(), UpdateInformationActivity.class);
+        intent.putExtra("pid", uid);
+        intent.putExtra("type",type);
         startActivity(intent);
     }
 }
