0d20d4966fdb7a4e1966ed32404d263200e47510
Hahahhaha12345 <92177243+Hahahhaha12345@users.noreply.github.com>
2023-11-12 22:34:17 -0500

    Update SettingsActivity.java

--- app/src/main/java/com/example/coen390_safehit/view/SettingsActivity.java
+++ app/src/main/java/com/example/coen390_safehit/view/SettingsActivity.java
@@ -1,23 +1,26 @@
 package com.example.coen390_safehit.view;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import com.example.coen390_safehit.R;
-
-public class SettingsActivity extends AppCompatActivity {
+	@@ -14,46 +12,34 @@ public class SettingsActivity extends AppCompatActivity {
 
     // Person id to identify user
     private String uid;
     private String type;
+    // Toolbar for back navigation
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        // Setup the toolbar
+        setupToolBar();
         // Fetch the id from the previous activity
         uid = getIntent().getStringExtra("pid");
         type= getIntent().getStringExtra("type");
@@ -26,12 +29,29 @@ public class SettingsActivity extends AppCompatActivity {
     public void onLogOutClicked(View view) {
         goToSignIn();
     }
+
     public void onDeleteAccountClicked(View view) {
         // Delete account from database
     }
     public void onUpdateInfoClicked(View view) {
         goToPersonalInformation();
     }
+
+    private void setupToolBar() {
+        toolbar = findViewById(R.id.toolbarSettings);
+        toolbar.setTitle("Settings");
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        //Return to previous page
+        toolbar.setNavigationOnClickListener(v -> finish());
+
+    }
+
     public void goToSignIn() {
         Intent intent = new Intent(getApplicationContext(), SignIn.class);
         startActivity(intent);
@@ -42,4 +62,5 @@ public class SettingsActivity extends AppCompatActivity {
         intent.putExtra("type",type);
         startActivity(intent);
     }
+        
 }
