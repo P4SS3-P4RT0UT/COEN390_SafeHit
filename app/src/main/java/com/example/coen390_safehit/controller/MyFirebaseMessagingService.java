8ffd59a29bd395d59c8a772aa863882460bee4ff
Laurie Anne Laberge <laberge.la@gmail.com>
2023-11-17 10:25:43 -0500

    Established communication between device and Firebase cloud messaging

---
+++ app/src/main/java/com/example/coen390_safehit/controller/MyFirebaseMessagingService.java
@@ -0,0 +1,24 @@
+package com.example.coen390_safehit.controller;
+
+import android.app.Service;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String token) {
+        super.onNewToken(token);
+        Log.e("newDeviceToken", token);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+
+
+
+}
